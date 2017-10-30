1cc9eb1b8e84a66efa04fa386ec09c0edd3b5d0f
Hans-Christian Payer <hans@amy.local.local>
2017-10-31 00:26:09 +0100

    mails werden leer gesendet, hat aber schon funktioniert.... :-(

--- src/app/phpmailer/phpmailer.component.ts
+++ src/app/phpmailer/phpmailer.component.ts
@@ -9,32 +9,32 @@ import { DateAdapter, NativeDateAdapter } from '@angular/material';
   styleUrls: ['./phpmailer.component.css']
 })
 export class PhpmailerComponent {
-  // resForm: FormGroup;
+  resForm: FormGroup;
   title = 'Zimmerreservierung';
   message: IMessage = {};
   
 
 
-  resForm = new FormGroup({
+/*   resForm = new FormGroup({
     name : new FormControl('', [Validators.required]),
     email : new FormControl('', [Validators.required, Validators.email]),
     message : new FormControl(''),
     anreise : new FormControl('', [Validators.required]),
     abreise : new FormControl('', [Validators.required])
 
-  });
+  }); */
 
   // email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor(private appService: PhpmailerService, dateAdapter: DateAdapter<NativeDateAdapter>) {
+  constructor(private appService: PhpmailerService, dateAdapter: DateAdapter<NativeDateAdapter>, fb: FormBuilder) {
     dateAdapter.setLocale('de-DE');
-    /* this.resForm =  fb.group({
-        'IMessage.name': [null, Validators.required],
-        'IMessage.email': [null, Validators.email],
-        'IMessage.message': [null],
-        'IMessage.anreise': [null, Validators.required],
-        'IMessage.abreise': [null, Validators.required]
-    }); */
+    this.resForm = fb.group ({
+        'message.name': [null, Validators.required],
+        'message.email': [null, Validators.email],
+        'message.message': [null],
+        'message.anreise': [null, Validators.required],
+        'message.abreise': [null, Validators.required]
+    });
   }
 
   sendEmail(message: IMessage) {
