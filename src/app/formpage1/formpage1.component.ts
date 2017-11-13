4d34ade64a765405c0564db11b9c9b4509969bff
Payer Hans-Christian2 <hans@net-so.org>
2017-11-13 18:10:14 +0100

    funzz das zeite formular

--- src/app/formpage1/formpage1.component.ts
+++ src/app/formpage1/formpage1.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, Validators} from '@angular/forms';
 import { Formpage1Service, IMessage1 } from './formpage1.service';
 
 @Component({
@@ -11,8 +12,14 @@ export class Formpage1Component {
   title = 'New PHP Email Example!';
   message: IMessage1 = {};
 
+  nameFormControl = new FormControl ('', [  Validators.required]);
+
   constructor(private appService: Formpage1Service) {
 
+  }
+
+  sendEmailTest(message: IMessage1) {
+    console.log(message.name);
   }
 
   sendEmail(message: IMessage1) {
